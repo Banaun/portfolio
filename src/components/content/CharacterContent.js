b095a55c0fe0e6044513193eca7a21c29bdc2e17
Emil Mogensen <89841651+Banaun@users.noreply.github.com>
2023-01-03 10:38:37 +0100

    Update CharacterContent.js

--- src/components/content/CharacterContent.js
+++ src/components/content/CharacterContent.js
@@ -3,10 +3,10 @@ function CharacterContent() {
     <div className='flex flex-col items-center h-full w-full p-[0.35rem] pb-5 md:p-2 md:pb-8 lg:p-4 lg:pb-12'>
       <span className='text-xs md:text-base lg:text-lg'>About</span>
       <span className='text-[5px] md:text-[8px] lg:text-[11px]'>
-        Hej jag heter Emil
+        Hej hej
       </span>
       <span className='text-[5px] md:text-[8px] lg:text-[11px]'>
-        Jag är en stor och stark kille
+        Test test
       </span>
     </div>
   );
